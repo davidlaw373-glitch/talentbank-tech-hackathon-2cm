f132246fd69e462da23b5f4e40d7363400646e80
XChin <chinmanhim2005@gmail.com>
2026-07-26 14:22:51 +0800

    feat: add complete employer interview management view

---
+++ src/app/employer/interviews/all/page.tsx
@@ -0,0 +1,5 @@
+import { AllInterviewsManagement } from "@/components/features/employer/interviews/all-interviews-management";
+
+export default function AllEmployerInterviewsPage() {
+  return <AllInterviewsManagement />;
+}
